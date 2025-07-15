55927b65db653955a1380f97ff5b46c84904a6d0
04Marsha <161226959+04Marsha@users.noreply.github.com>
2025-07-15 22:27:33 +0530

    updates-adding-mean-cert

--- src/app/Certificates/certificates.component.ts
+++ src/app/Certificates/certificates.component.ts
@@ -21,6 +21,7 @@ export class CertificatesComponent implements AfterViewInit {
   @ViewChild('nextBtn') nextBtnRef!: ElementRef;
   certificates = [
     {img: 'certificates/angular.jpg'},
+    {img: 'certificates/MEAN.jpg'},
     {img: 'certificates/pygame.jpg'},
   ]
 
