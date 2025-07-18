ab89f4ad58dfb5d6c83c1cc0bbeab46abd8673a0
04Marsha <161226959+04Marsha@users.noreply.github.com>
2025-07-18 22:45:52 +0530

    adding-certificate-route

--- src/app/navLinks/certs/certs.component.ts
+++ src/app/navLinks/certs/certs.component.ts
@@ -11,6 +11,47 @@ export class CertsComponent implements OnInit {
   ngOnInit(): void {
     setTimeout(() => {
       this.isLoading = false;
-    }, 1000); //scrolling time
+    }, 0); //scrolling time
+  }
+  certificates = [
+    {
+      title: 'MEAN Stack Development',
+      description: 'Completed from Udemy',
+      imageUrl: 'certificates/MEAN.jpg',
+    },
+    {
+      title: 'Angular',
+      description: 'Completed from Udemy',
+      imageUrl: 'certificates/angular.jpg',
+    },
+    {
+      title: 'Getting started with Artificial Intelligence',
+      description: 'Completed from IBM',
+      imageUrl: 'certificates/getting_started_with_AI.jpg',
+    },
+    {
+      title: 'Journey to Cloud',
+      description: 'Completed from IBM',
+      imageUrl: 'certificates/journey_to_cloud.jpg',
+    },
+    {
+      title: 'Python for Game Programming',
+      description: 'Certificate from Udemy',
+      imageUrl: 'certificates/pygame.jpg',
+    },
+  ];
+
+  modalOpen = false;
+  modalImage = '';
+  modalTitle = '';
+
+  openModal(imageUrl: string, title: string): void {
+    this.modalImage = imageUrl;
+    this.modalTitle = title;
+    this.modalOpen = true;
+  }
+
+  closeModal(): void {
+    this.modalOpen = false;
   }
 }
