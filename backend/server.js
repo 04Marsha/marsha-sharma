8c61fc9ef751ec72f9af35160a8e154292a08bc1
04Marsha <161226959+04Marsha@users.noreply.github.com>
2025-10-28 12:30:49 +0530

    backend url fix

--- backend/server.js
+++ backend/server.js
@@ -17,8 +17,8 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
-app.use("/api/contact", contactRoutes);
-app.use("/api/feedback", feedbackRoutes);
+app.use("/contact", contactRoutes);
+app.use("/feedback", feedbackRoutes);
 
 const PORT = process.env.PORT || 5000;;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
