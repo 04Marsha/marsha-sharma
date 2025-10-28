0c499d80645f8afbc390f47378369081dddede4f
04Marsha <161226959+04Marsha@users.noreply.github.com>
2025-10-28 16:31:58 +0530

    migrated to sendgrid... local testing done

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -2,7 +2,7 @@ import sgMail from "@sendgrid/mail";
 import dotenv from "dotenv";
 dotenv.config();
 
-sgMail.setApiKey(process.env.SENDGRID_APIKEY);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const SENDER_MAIL = process.env.EMAIL_USER;
 
